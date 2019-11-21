5862caa8dbc23342537b0578a71eee480d82c7a3
18ljones <39415738+18ljones@users.noreply.github.com>
2019-11-21 14:55:52 -0600

    client-side modifications

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,52 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var movieEntries = document.getElementsByClassName("movie-entry");
+
+var form = document.getElementById("search-and-filter-form");
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var i, entry;
+
+    var term = document.getElementById("search").value;
+
+    var mpaa = [];
+    var mpaaCheckBoxes = document.getElementsByClassName("mpaa");
+
+    var minIMDB = document.getElementsByClassName("minIMDB");
+
+    for (var j = 0; j < mpaaCheckBoxes.length; j++) {
+        if (mpaaCheckBoxes[j].checked) {
+            mpaa.push(mpaaCheckBoxes[j].value);
+        }
+    }
+
+    for (i = 0; i < movieEntries.length; i++) {
+
+        //unhide all entries
+        entry = movieEntries[i];
+        entry.style.display = "block";
+
+        //by search term
+        if (term) {
+            if (!entry.dataset.title.toLowerCase().includes(term.toLowerCase())) {
+                entry.style.display = "none";
+            }
+        }
+
+        //by mpaa rating
+        if (mpaa.length > 0) {
+            if (!mpaa.includes(entry.dataset.mpaa)) {
+                entry.style.display = "none";
+            }
+        }
+
+        //by imdb rating
+        if (minIMDB) {
+            if (!entry.dataset.imdb || parseFloat(minIMDB) > parseFloat(entry.dataset.imdb)) {
+                entry.style.display = "none";
+            }
+        }
+    }
+});
